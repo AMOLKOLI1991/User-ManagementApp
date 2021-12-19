37bb219ebee6c65fddd2b083082528a91fa227fb
AMOLKOLI1991 <amolkoli595@gmail.com>
2021-12-19 17:10:25 +0530

    UMA-10:create and implement emailutils

--- src/main/java/com/bikkadit/usermanagement/util/EmailUtil.java
+++ src/main/java/com/bikkadit/usermanagement/util/EmailUtil.java
@@ -10,5 +10,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailUtil {
 
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
+	public boolean sendMail(String to, String subject, String body) throws Exception {
+		boolean isSent = false;
+		
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+		messageHelper.setTo(to);
+		messageHelper.setSubject(subject);
+		messageHelper.setText(body, true);
+		javaMailSender.send(mimeMessage);
+		isSent=true;
+		return isSent;
+	}
+
 }
